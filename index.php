d9ef3a188579cb5572b21b252615e3ac2c447144
Manuel Quijano <quijaman1988@gmail.com>
2016-02-10 22:14:19 -0600

    Finished with the HTML coding

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<html>
+
 <head>
   <title>Project 1</title>
   <meta charset="utf-8">
@@ -9,18 +11,18 @@
 </head>
 
 
-<div style="border-style:solid; border-width:2px; border-color:#EFEFEF; padding:15px;font-size:150%">
+<div style="padding:15px;font-size:150%">
 
 <div class="row">
-  <div class="col-md-12"><h4 style="font-size:110%">Raúl Franchi</h4></div>
+  <div class="col-md-12"><h4 style="font-size:110%">Manuel Quijano</h4></div>
 </div>
 <div class="row">
-  <div class="col-md-2"><img src="images/raul.jpg" alt="" /></div>
-  <div class="col-md-5"><br><span style="font-size:75%;font-weight:bold">Experto en alta dirección y en formación humana</span><br><h6> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ciudad de México</h6></div>
+  <div class="col-md-2"><img class="img-circle" src="images/manuel.jpg" alt="" /></div>
+  <div class="col-md-5"><br><span style="font-size:75%;font-weight:bold">Web Developer</span><h6> &nbsp;Mexico City</h6></div>
  
   <div class="col-md-1"></div>
  <div class="col-md-2"></div>
-  <div class="col-md-2"><br><h5 style="font-size:65%">M 52 (55)2980 8640</h5><h5 style="font-size:65%">D  52 (55) 2222 2521</h5><h5 style="font-size:65%">@raul_franchi</h5></div>
+  <div class="col-md-2"><br><h5 style="font-size:65%">M 52 (55) 2185 8579</h5><h5 style="font-size:65%">D  52 (55) 2222 2987</h5><h5 style="font-size:65%">@manuel_quijano</h5></div>
   
 </div>
 
@@ -35,8 +37,8 @@
 </div>
 <div class="row">
   <div class="col-md-2"></div>
-  <div  class="col-md-8"><span style="font-size:80%;background-color:#3D8EB9; color:white; padding:0; margin-left:5; text-align:center; padding:5px 40px 5px 40px"><a href="https://twitter.com/raul_franchi" style="color:white">Follow</a></span>
-  <span style="font-size:80%;background-color:#3D8EB9; color:white; padding:0; margin-left:5; text-align:center; padding:5px 40px 5px 40px"><a href="http://raulfranchi.com/index.php/contact" style="color:white">Mensaje</a></span>&nbsp;&nbsp;&nbsp;<h4>raulfranchi@cineticaeducativa.com</h4></div>
+  <div  class="col-md-8"><span style="font-size:80%;background-color:#3D8EB9; color:white; padding:0; margin-left:5; text-align:center; padding:5px 40px 5px 40px"><a href="#" style="color:white">Follow</a></span>
+  <span style="font-size:80%;background-color:#3D8EB9; color:white; padding:0; margin-left:5; text-align:center; padding:5px 40px 5px 40px"><a href="#" style="color:white">Message</a></span><h4>&nbsp;quijaman1988@gmail.com</h4></div>
 
  <div class="col-md-2"></div>
 
@@ -53,23 +55,22 @@
 
 <div style="border-style:solid; border-width:2px; border-color:#EFEFEF; padding:15px; border-bottom:none">
 <div class="row">
-  <div class="col-md-2"><h4>SEMBLANZA</h4></div>
-  <div class="col-md-10">Una trayectoria poco frecuente. Por un lado posee amplia experiencia empresarial y ha ocupado posiciones directivas en las áreas comercial y de marketing. Por otro lado es filósofo de la empresa, con dos maestrías y un doctorado.</div>
+  <div class="col-md-2"><h4>Project 2</h4></div>
+  <div class="col-md-10"><a href="#">p2.manuelquijano.com</a><br><a href="#">Github</a></div>
 </div>
 </div>
 
 <div style="border-style:solid; border-width:2px; border-color:#EFEFEF; padding:15px;border-bottom:none">
 <div class="row">
-  <div class="col-md-2"><h4>EXPERTO</h4></div>
-  <div class="col-md-10">Es experto en ética aplicada y se especializa en la adquisición de hábitos de alto desempeño, el cambio cultural y la integración de mejores prácticas socialmente responsables con los resultados financieros..</div>
+  <div class="col-md-2"><h4>Project 3</h4></div>
+  <div class="col-md-10"><a href="#">p3.manuelquijano.com</a><br><a href="#">Github</a></div>
 </div>
 </div>
 
-<div style="border-style:solid; border-width:2px; border-color:#EFEFEF; padding:15px;border-bottom:none">
+<div style="border-style:solid; border-width:2px; border-color:#EFEFEF; padding:15px;">
 <div class="row">
-  <div class="col-md-2"><h4>SPEAKER</h4></div>
-  <div class="col-md-10">Como speaker profesional, entrena 5,000 directores generales y funcionales por año en diversos programas de perfeccionamiento humano y directivo, principalmente en IPADE y como conferencista en eventos corporativos.</div>
-</div>
+  <div class="col-md-2"><h4>Project 4</h4></div>
+  <div class="col-md-10"><a href="#">p3.manuelquijano.com</a><br><a href="#">Github</a></div>
 </div>
 
 
